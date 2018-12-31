244dd2f0d1344c5c2e00ee47a8854a22e2114577
Simon Hampel <simon@hampelgroup.com>
2018-12-31 20:06:09 +1100

    added example configuration

--- src/config/envroute.php
+++ src/config/envroute.php
@@ -7,6 +7,27 @@ return array(
 
 	'packages' => [
 
+// Example configuration
+//
+// key = value used for APP_ENV
+// path = path to where the package is installed - can be anywhere
+// providers = list of service providers to load, as would normally be put in config('app.providers')
+// aliases = list of Facade aliases to load, as would normally be put in config('app.aliases')
+//
+// If this is not a Laravel application, then only the path value is required
+//
+//		'test' => [
+//			'path' => base_path() . '/packages/test',
+//
+//			'providers' => [
+//				'Hampel\Test\TestServiceProvider',
+//			],
+//
+//			'aliases' => [
+//				'Test' => 'Hampel\Test\Facades\Test',
+//			],
+//		],
+
 	],
 
 );
