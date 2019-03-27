8a4c8bf9f6d9510865146ac1813235c5ba5723fa
Simon Hampel <simon@hampelgroup.com>
2019-03-27 11:31:00 +1100

    rewrote EnvRoute\Bootstrap\LoadEnvironmentVariables to be a subclass of Illuminate\Foundation\Bootstrap\LoadEnvironmentVariables and simplify operation by augmenting the functionality of function checkForSpecificEnvironmentFile to identify our route based env

--- src/Bootstrap/LoadEnvironmentVariables.php
+++ src/Bootstrap/LoadEnvironmentVariables.php
@@ -1,54 +1,40 @@
 <?php namespace EnvRoute\Bootstrap;
 
-use Dotenv\Dotenv;
-use Dotenv\Exception\InvalidPathException;
-use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Foundation\Bootstrap\LoadEnvironmentVariables as BaseLoadEnvironmentVariables;
 
-class LoadEnvironmentVariables {
+class LoadEnvironmentVariables extends BaseLoadEnvironmentVariables {
 
-	/**
-	 * Bootstrap the given application.
-	 *
-	 * @param  \Illuminate\Contracts\Foundation\Application  $app
-	 * @return void
-	 */
-	public function bootstrap(Application $app)
+	protected function checkForSpecificEnvironmentFile($app)
 	{
-		$app->detectEnvironment(function() use ($app)
-		{
-			if (!isset($_SERVER['REQUEST_URI'])) return $this->loadEnvironment($app);
+        if ($app->runningInConsole() && ($input = new ArgvInput)->hasParameterOption('--env')) {
+            if ($this->setEnvironmentFilePath(
+                $app, $app->environmentFile().'.'.$input->getParameterOption('--env')
+            )) {
+                return;
+            }
+        }
 
+        // EnvRoute uses the first segment of the path to determine the environment to load
+		if (isset($_SERVER['REQUEST_URI']))
+		{
 			$uri = trim($_SERVER['REQUEST_URI'], '/');
-			if (empty($uri)) return $this->loadEnvironment($app);
-
-			$paths = explode('/', $uri);
-
-			return $this->loadEnvironment($app, $paths[0]);
-		});
-	}
-
-	protected function loadEnvironment(Application $app, $path = "")
-	{
-		if (!empty($path))
-		{
-			$file = ".env.{$path}";
-
-			if (file_exists($app->environmentPath().'/'.$file))
+			if (!empty($uri))
 			{
-				$app->loadEnvironmentFrom(".env.{$path}");
+				$paths = explode('/', $uri);
+				if ($this->setEnvironmentFilePath($app, $app->environmentFile() . '.' . $paths[0]))
+				{
+					return;
+				}
 			}
 		}
 
-		try
-		{
-			(new Dotenv($app->environmentPath(), $app->environmentFile()))->load();
-		}
-		catch (InvalidPathException $e)
-		{
-			die($e->getMessage());
-		}
+        if (! env('APP_ENV')) {
+            return;
+        }
 
-		return $path;
+        $this->setEnvironmentFilePath(
+            $app, $app->environmentFile().'.'.env('APP_ENV')
+        );
 	}
 
 }
